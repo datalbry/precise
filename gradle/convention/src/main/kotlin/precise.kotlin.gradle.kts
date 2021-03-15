02f54a0823ebc9598ebc97be4e520b1387a22051
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-03-15 17:07:21 +0100

    [KC-T-14] Refactor build.script to use includeBuild with conventions

---
+++ gradle/convention/src/main/kotlin/precise.kotlin.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    `java-library`
+    idea
+    jacoco
+}
+
+version = "0.0.1-SNAPSHOT"
+group = "io.datalbry.precise"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    google()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+// Do not generate reports for individual projects
+tasks.getByName("jacocoTestReport") {
+    enabled = false
+}
+
+// Prefix all of our jars with the company name
+tasks.withType<AbstractArchiveTask> {
+    archiveBaseName.set("lbrary-${getArchiveName(this.project)}")
+    archiveVersion.set(this.project.version.toString())
+}
+
+// We are using Kotlin, so wdk about the Java Version onwards, as we are not relying on Java 11+ features
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+// We are using jUnit5 as default
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+//Function to calculate unique archive names,
+//since we are not prefixing all of our submodules with the parent hierarchy.
+fun getArchiveName(project: Project): String {
+    var archiveName = project.name
+    var parent = project.parent
+    while (parent != null) {
+        archiveName = "${parent.name}-${archiveName}"
+        parent = parent.parent
+    }
+    return archiveName
+}
+
