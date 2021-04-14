a9ff748d029c48f3964d9d901fefa4d29f0fca7c
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-04-14 10:15:12 +0200

    [KC-T-35] Refactor dependencies to rely on dependency catalog

--- serialization-jackson/build.gradle.kts
+++ serialization-jackson/build.gradle.kts
@@ -4,14 +4,9 @@ plugins {
 }
 
 dependencies {
-    val jacksonVersion = "2.12.2"
-    val junit5Version = "5.7.1"
-
     api(project(":api"))
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation(libs.bundles.jackson)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
-    testImplementation("org.junit.vintage:junit-vintage-engine:$junit5Version")
+    testImplementation(libs.bundles.junit)
 }
