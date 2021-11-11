6ffd3fe2c85652ef7f12743c2078383d4441df9f
Zeljko Bekcic <bekciczeljko@gmail.com>
2021-11-11 15:19:37 +0100

    refactor: correct logging format

--- serialization-jackson/src/main/kotlin/io/datalbry/precise/serialization/jackson/exception/InvalidEnumValueException.kt
+++ serialization-jackson/src/main/kotlin/io/datalbry/precise/serialization/jackson/exception/InvalidEnumValueException.kt
@@ -3,4 +3,4 @@ package io.datalbry.precise.serialization.jackson.exception
 import io.datalbry.precise.api.schema.type.EnumType
 
 class InvalidEnumValueException(type: EnumType, value: String) :
-    IllegalArgumentException("Value \"${value}\" is not contained by type \"${type.name}\" with values: ${type.values}")
+    IllegalArgumentException("Value [${value}] is not contained by type [${type.name}] with values [${type.values}]")
