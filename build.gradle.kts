21954e593753640f312d4572438dfbe8ace4a012
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2022-01-06 14:51:20 +0100

    fix(catalog): upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,18 @@ plugins {
     id("datalbry.publish-internal")
     id("datalbry.publish-maven-central")
     id("io.datalbry.plugin.semver") version "0.2.2"
+    id("io.datalbry.catalog.updater") version "0.0.2"
     idea
 }
 
 semver {
     version("alpha", "alpha.{COMMIT_TIMESTAMP}")
     version("beta", "beta.{COMMIT_TIMESTAMP}")
+}
+
+catalogUpdater {
+    from = "./gradle/libs.versions.toml"
+    to = "./gradle/libs.versions.toml"
 }
 
 group = "io.datalbry.precise"
